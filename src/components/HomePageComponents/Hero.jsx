617bb5f48536cab4bf131b41d24c05bd52daad9b
Divya <divyamk.brigade@gmail.com>
2025-02-14 13:05:32 +0530

    Changed fonts section

--- src/components/HomePageComponents/Hero.jsx
+++ src/components/HomePageComponents/Hero.jsx
@@ -5,13 +5,12 @@ const Hero = () => {
     <div className="min-h-screen w-full bg-gradient-to-br from-black via-black to-blue-900/30 flex items-center justify-center text-center px-4">
       <div className="max-w-8xl mx-auto">
         <h1 className="text-5xl md:text-7xl font-bold text-transparent stroke-text mb-8 leading-tight">
-          Transform Your Digital Presence Where <br />
-          Strategy Meets Stunning Design
+          Your Vision,Our Obsession <br />
+          lets make it Iconic
         </h1>
 
         <p className="text-gray-300 text-lg md:text-xl max-w-2xl mx-auto leading-relaxed">
-          Grow smarter, grow faster as we need Solutions at the right place and at Smarttrak we are
-          empowering all your digital twin needs
+          Transforming visions into vibrant realities,one pixel at a time-crafting innovative digital experiences that elevate your brand.
         </p>
       </div>
 
